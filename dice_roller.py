ddbb6881ad1325264dbe9f9bff3a24f49978a113
MARS-XI <mars-xi@users.noreply.github.com>
2019-10-23 13:48:04 +0200

    added DX dice (arbitrary number of sides)

--- dice_roller.py
+++ dice_roller.py
@@ -7,16 +7,16 @@ D&D Dice Roller with roll modifiers
 
 # Built-in/Generic Imports
 import random
-
 # Libs
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
 
 __author__ = 'Marcello Lamonaca'
 __copyright__ = 'Copyright 2019, Marcello Lamonaca'
 __credits__ = ['Marcello Lamonaca']
 __license__ = 'MIT'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __maintainer__ = 'Marcello Lamonaca'
 __email__ = ''
 __status__ = 'dev'
@@ -81,6 +81,14 @@ def D100_roll():
     roll_result.set(DX(D100_rolls.get(), 100, D100_modifier_var.get()))
 
 
+def DX_roll():
+    """calls DX with a user inserted number of sides"""
+    try:
+        roll_result.set(DX(DX_rolls.get(), DX_sides.get(), DX_modifier_var.get()))
+    except:
+        messagebox.showerror(title='ERROR', message='the number of sides must be an integer')
+
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title('Dice Roller')
@@ -104,7 +112,7 @@ if __name__ == '__main__':
     D4_label = ttk.Label(input_frame, text='D4')
     D4_label.grid(column=1, row=1, padx=label_padx, pady=std_pady)
     D4_rolls = tk.IntVar()
-    D4_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D4_rolls, wrap=True)
+    D4_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D4_rolls, wrap=True)
     D4_spinbox.grid(column=2, row=1, padx=entry_padx, pady=std_pady)
     D4_result = tk.IntVar()
     D4_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -119,7 +127,7 @@ if __name__ == '__main__':
     D6_label = ttk.Label(input_frame, text='D6')
     D6_label.grid(column=1, row=2, padx=label_padx, pady=std_pady)
     D6_rolls = tk.IntVar()
-    D6_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D6_rolls, wrap=True)
+    D6_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D6_rolls, wrap=True)
     D6_spinbox.grid(column=2, row=2, padx=entry_padx, pady=std_pady)
     D6_result = tk.IntVar()
     D6_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -134,7 +142,7 @@ if __name__ == '__main__':
     D8_label = ttk.Label(input_frame, text='D8')
     D8_label.grid(column=1, row=3, padx=label_padx, pady=std_pady)
     D8_rolls = tk.IntVar()
-    D8_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D8_rolls, wrap=True)
+    D8_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D8_rolls, wrap=True)
     D8_spinbox.grid(column=2, row=3, padx=entry_padx, pady=std_pady)
     D8_result = tk.IntVar()
     D8_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -149,7 +157,7 @@ if __name__ == '__main__':
     D10_label = ttk.Label(input_frame, text='D10')
     D10_label.grid(column=1, row=4, padx=label_padx, pady=std_pady)
     D10_rolls = tk.IntVar()
-    D10_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D10_rolls, wrap=True)
+    D10_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D10_rolls, wrap=True)
     D10_spinbox.grid(column=2, row=4, padx=entry_padx, pady=std_pady)
     D10_result = tk.IntVar()
     D10_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -164,7 +172,7 @@ if __name__ == '__main__':
     D12_label = ttk.Label(input_frame, text='D12')
     D12_label.grid(column=1, row=5, padx=label_padx, pady=std_pady)
     D12_rolls = tk.IntVar()
-    D12_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D12_rolls, wrap=True)
+    D12_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D12_rolls, wrap=True)
     D12_spinbox.grid(column=2, row=5, padx=entry_padx, pady=std_pady)
     D12_result = tk.IntVar()
     D12_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -179,7 +187,7 @@ if __name__ == '__main__':
     D20_label = ttk.Label(input_frame, text='D20')
     D20_label.grid(column=1, row=6, padx=label_padx, pady=std_pady)
     D20_rolls = tk.IntVar()
-    D20_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D20_rolls, wrap=True)
+    D20_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D20_rolls, wrap=True)
     D20_spinbox.grid(column=2, row=6, padx=entry_padx, pady=std_pady)
     D20_result = tk.IntVar()
     D20_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -194,7 +202,7 @@ if __name__ == '__main__':
     D100_label = ttk.Label(input_frame, text='D100')
     D100_label.grid(column=1, row=7, padx=label_padx, pady=std_pady)
     D100_rolls = tk.IntVar()
-    D100_spinbox = tk.Spinbox(input_frame, from_=0, to=100, textvariable=D100_rolls, wrap=True)
+    D100_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=D100_rolls, wrap=True)
     D100_spinbox.grid(column=2, row=7, padx=entry_padx, pady=std_pady)
     D100_result = tk.IntVar()
     D100_modifier_label = ttk.Label(input_frame, text='MODIFIER')
@@ -205,7 +213,23 @@ if __name__ == '__main__':
     D100_button = ttk.Button(input_frame, text='ROLL', command=D100_roll)
     D100_button.grid(column=10, row=7, padx=button_padx, pady=std_pady)
 
-    # TODO: add DX roll
+    # DX INPuT AND BUTTON
+    DX_label = ttk.Label(input_frame, text='D')
+    DX_label.grid(column=0, row=8, padx=0, pady=std_pady)
+    DX_sides = tk.IntVar()
+    DX_sides_entry = ttk.Entry(input_frame, textvariable=DX_sides, width=5)
+    DX_sides_entry.grid(column=1, row=8, pady=std_pady)
+    DX_rolls = tk.IntVar()
+    DX_spinbox = ttk.Spinbox(input_frame, from_=1, to=100, textvariable=DX_rolls, wrap=True)
+    DX_spinbox.grid(column=2, row=8, padx=entry_padx, pady=std_pady)
+    DX_result = tk.IntVar()
+    DX_modifier_label = ttk.Label(input_frame, text='MODIFIER')
+    DX_modifier_label.grid(column=3, row=8, padx=label_padx)
+    DX_modifier_var = tk.StringVar()
+    DX_modifier_entry = ttk.Entry(input_frame, textvariable=DX_modifier_var)
+    DX_modifier_entry.grid(column=4, row=8, padx=entry_padx)
+    DX_button = ttk.Button(input_frame, text='ROLL', command=DX_roll)
+    DX_button.grid(column=10, row=8, padx=button_padx, pady=std_pady)
 
     # FRAME FOR OUTPUTS
     output_frame = ttk.Frame(root)
