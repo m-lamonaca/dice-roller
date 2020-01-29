939a27cbf889d5135a855d321704736c3afafbba
Marcello Lamonaca <32648168+MARS-XI@users.noreply.github.com>
2020-01-29 22:41:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,17 +8,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    # This is the name of your project. The first time you publish this
-    # package, this name will be registered for you. It will determine how
-    # users can install this project, e.g.:
-    #
-    # $ pip install sampleproject
-    #
-    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
-    #
-    # There are some restrictions on what makes a valid project name
-    # specification here:
-    # https://packaging.python.org/specifications/core-metadata/#name
     name='gui-dice-roller',
 
     version='0.2.2',
